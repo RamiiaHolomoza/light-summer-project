b6adf3432912978f97dbf56d2b38513bb54dad46
Oleksandr Kurchenko <151569683+AlexSaymon@users.noreply.github.com>
2024-08-16 20:54:27 -0500

    1

--- src/js/projects.js
+++ src/js/projects.js
@@ -3,16 +3,44 @@ import 'swiper/swiper-bundle.css';
 
 const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
-  loop: true,
   pagination: {
     el: '.swiper-pagination',
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.custom-button-next',
+    prevEl: '.custom-button-prev',
   },
 });
 
-document.querySelector('.custom-button-next').addEventListener('click', () => {
-  swiper.slideNext();
-});
+// Log the values of activeSlideIndex and totalSlideAmount
+
+function btnDisable() {
+  const btnNext = document.querySelector('.custom-button-next');
+  const btnPrev = document.querySelector('.custom-button-prev');
+  const totalSlideAmount = swiper.slides.length;
+  const activeSlideIndex = swiper.activeIndex;
+
+  console.log(
+    `Active Slide Index: ${activeSlideIndex}, Total Slides: ${totalSlideAmount}`
+  );
+
+  if (activeSlideIndex === totalSlideAmount - 1) {
+    btnNext.disabled = true;
+    btnNext.classList.add('nav-button-disabled');
+  } else {
+    btnNext.disabled = false;
+    btnNext.classList.remove('nav-button-disabled');
+  }
+
+  if (activeSlideIndex === 0) {
+    btnPrev.disabled = true;
+    btnPrev.classList.add('nav-button-disabled');
+  } else {
+    btnPrev.disabled = false;
+    btnPrev.classList.remove('nav-button-disabled');
+  }
+}
+
+swiper.on('toEdge', btnDisable);
+
+btnDisable();
