ff231dccd426f8309096029f20e7e57a3a7b45c1
RamiiaHolomoza <162986900+RamiiaHolomoza@users.noreply.github.com>
2024-08-19 18:28:57 +0300

    add js cover

--- src/js/cover.js
+++ src/js/cover.js
@@ -1,14 +1,41 @@
-const targetElement = document.querySelector('.container-covers');
+// const targetElement = document.querySelector('.container-covers');
 
-function handleScroll() {
-  const windowHeight = window.innerHeight;
-  const elementRect = targetElement.getBoundingClientRect();
-  const elementTop = elementRect.top;
+// function handleScroll() {
+//   const windowHeight = window.innerHeight;
+//   const elementRect = targetElement.getBoundingClientRect();
+//   const elementTop = elementRect.top;
 
-  if (elementTop < windowHeight * 0.5) {
-    // Элемент у середині viewport
-    targetElement.classList.add('covers-project'); // класс з анимацією
-  }
+//   if (elementTop < windowHeight * 0.5) {
+//     // Элемент у середині viewport
+//     targetElement.classList.add('covers-project'); // класс з анимацією
+//   }
+// }
+const coversSection = document.querySelector('.section-covers');
+window.addEventListener('scroll', handleScroll);
+
+function startAnimation(entries, observer) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      document.querySelectorAll('.covers-project').forEach(item => {
+        item.style.animationPlayState = 'running';
+      });
+    } else {
+      document.querySelectorAll('.covers-project').forEach(item => {
+        item.style.animationPlayState = 'paused';
+      });
+    }
+  });
 }
 
-window.addEventListener('scroll', handleScroll);
+const observerOptions = {
+  root: null, // Використовуємо вікно браузера як viewport
+  threshold: 0.1, // Анімація запускається, коли 10% секції видимі
+};
+
+const observer = new IntersectionObserver(startAnimation, observerOptions);
+
+observer.observe(coversSection); // Відстежуємо секцію covers
+
+document.querySelectorAll('.covers-project').forEach(item => {
+  item.style.animationPlayState = 'paused';
+});
