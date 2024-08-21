cd5a7d8a22e1d6a9060e8cf18aa35fff595ab0a8
Masik091 <maliastep155@gmail.com>
2024-08-21 09:06:30 +0300

    update

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,5 +1,7 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
+import svgrootBasis from '../img/icons.svg' 
+
 export {questions, ulElement, createFAQMarkup, styleElement};
 
 new Accordion('.faq-container', {
@@ -12,7 +14,7 @@ new Accordion('.faq-container', {
 const questions = [
     {
   svgroot:
-   './img/icons.svg#icon-Icon-bottom' ,
+   '#icon-Icon-bottom' ,
   topic:
   'What programming languages ​​are most often used in your project?',
   text:
@@ -20,7 +22,7 @@ const questions = [
   },
     {
   svgroot:
-   './img/icons.svg#icon-Icon-bottom' ,
+   '#icon-Icon-bottom' ,
   topic:
   'What are the deadlines for the project?',
   text:
@@ -29,7 +31,7 @@ const questions = [
 
   {
     svgroot:
-     './img/icons.svg#icon-Icon-bottom' ,
+     '#icon-Icon-bottom' ,
     topic:
     'What payment methods do you accept?',
     text:
@@ -37,7 +39,7 @@ const questions = [
     },
 
     {svgroot:
-     './img/icons.svg#icon-Icon-bottom' ,
+     '#icon-Icon-bottom' ,
     topic:
     'How can I contact you?',
     text:
@@ -45,7 +47,7 @@ const questions = [
     },
 
     {svgroot:
-        './img/icons.svg#icon-Icon-bottom' ,
+        '#icon-Icon-bottom' ,
        topic:
        'Do you provide advice or support?',
        text:
@@ -53,9 +55,9 @@ const questions = [
        },
 
        {svgroot:
-        '../img/icons.svg#icon-Icon-bottom' ,
+        '#icon-Icon-bottom' ,
        topic:
-       'What does the process of developing a software product look like from idea to implementation?',
+       'What does the process of developing a software<br >product look like from idea to implementation?',
        text:
        'Payment through credit and debit cards such as Visa, MasterCard, specialized electronic payment systems such as PayPal, as well as payments in cryptocurrencies such as Bitcoin, Ethereum and others.',
        },
@@ -71,9 +73,9 @@ const questions = [
           <li class="faq ${index !== questions.length - 1 ? '' : ''}">
             <div class="faq-list-up">
                <h3 class="question">${topic}</h3>
-                 <svg class="faq-icon">
-                   <use href="${svgroot}"></use>
-                </svg>
+                 <p class="faq-icon"><svg width="20" height="20">
+                   <use href="${svgrootBasis}${svgroot}" ></use>
+                </svg></p>
            </div>
           <p class="faq-text">${text}</p>
           </li>`
@@ -83,23 +85,25 @@ const questions = [
   }
   ulElement.insertAdjacentHTML('beforeend', createFAQMarkup(questions));
 
-  function handleCardClick(event) {
+function handleCardClick(event) {
     if (event.target.closest('.faq-icon')) { 
       const faqItem = event.target.closest('.faq');
       const faqText = faqItem.querySelector('.faq-text');
-  
+      const faqIcon = faqItem.querySelector('.faq-icon');
+      
       faqItem.classList.toggle('active');
+      faqIcon.classList.toggle('rotate');
   
       if (faqItem.classList.contains('active')) {
-        faqText.style.maxHeight = faqText.scrollHeight + 'px'; 
+        faqText.style.maxHeight = faqText.scrollHeight + 'px';  
       } else {
         faqText.style.maxHeight = 0; 
       }
     }
+
   }
   
   ulElement.addEventListener('click', handleCardClick);
 
 const styleElement = document.createElement('style');
 document.head.appendChild(styleElement);
-import 'accordion-js/dist/accordion.min.css';
