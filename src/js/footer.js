d991263f15d2b14311cb338f0b1e02ecfd554418
Inna Ratushna <Ratushnaya.uv@gmail.com>
2024-08-22 00:30:19 +0300

    add and fixed code

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,19 +1,12 @@
 
-document.addEventListener("DOMContentLoaded", function() {
-    const form = document.querySelector('.footer-form');
+ const form = document.querySelector('.footer-form');
     const modal = document.getElementById('modal');
-  const closeModalBtn = document.querySelector('.footer-close-button');
-  
-  // Відстеження події на відправлення форми
-    form.addEventListener('submit', function(event) {
-      event.preventDefault(); // Перешкоджаємо стандартному відправленню форми
-      
-       // Перевірка валідності форми
-      if (form.checkValidity()) {
-        // Якщо форма валідна, відображаємо модальне вікно
-        modal.classList.add('is-open');
-          
-        const STORAGE_KEY = "feedback-form-state";
+    const closeModalBtn = document.querySelector('.footer-close-button');
+    const openModalBtn = document.querySelector('.footer-form-button');
+    const hoverOpenBtn = document.querySelector('.open-btn-hover');
+
+
+const STORAGE_KEY = "feedback-form-state";
 
         let formData = {
           email: "",
@@ -59,14 +52,53 @@ document.addEventListener("DOMContentLoaded", function() {
           }
           localStorage.removeItem(STORAGE_KEY)
 }
-      
-          form.reset();
-        
-      }
-    });
+// Функція для обробки введення даних у форму
+        function handleInput(event) {
+          const value = event.target.value.trim();
+          const key = event.target.name.trim();
 
-    // Закриття модального вікна
-    closeModalBtn.addEventListener('click', function() {
+          formData[key] = value;
+    
+          localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    
+          console.log(key, value);
+          // Перевірка введеної електронної пошти
+        if (key === "email") {
+            validateEmail(event.target);
+        }
+        }
+// Підтвердьте електронну адресу та надішліть коментар
+function validateEmail(inputElement) {
+  const emailMessage = document.getElementById("email-message");
+  const emailPattern = /^\w+(\.\w+)?@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+
+  if (emailPattern.test(inputElement.value.trim())) {
+    emailMessage.textContent = "Success!";
+    emailMessage.classList.add("success");
+    emailMessage.classList.remove("error");
+  } else {
+    emailMessage.textContent = "Invalid email, try again";
+    emailMessage.classList.add("error");
+    emailMessage.classList.remove("success");
+    emailMessage.style.color = '#e74a3b';
+  }
+
+}
+  
+
+      form.addEventListener('submit', function(event) {
+      event.preventDefault(); // Перешкоджаємо стандартному відправленню форми
+       // Перевірка валідності форми
+        if (form.checkValidity()) {
+         modal.classList.add('is-open');
+        } else {
+          hoverOpenBtn.classList.remove('open-btn-hover');
+        }
+         form.reset();
+      })
+
+// Закриття модального вікна
+        closeModalBtn.addEventListener('click', function() {
         modal.classList.remove('is-open');
     });
 
@@ -79,21 +111,10 @@ document.addEventListener("DOMContentLoaded", function() {
   
      //закрити модальне вікно при натисканні на Esc
 window.addEventListener('keydown', (e) => {
-    if (e.key === "Escape") {
-      modal.classList.remove('is-open');
-      
-    }
-})
-});
-
-
-
-
-
-
-
-
-
+  if (e.key === "Escape") {
+    modal.classList.remove('is-open');
+  }
+    })
 
 
 
